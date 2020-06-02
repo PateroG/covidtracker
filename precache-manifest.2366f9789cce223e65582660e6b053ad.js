297bd4822130ca875ad0c8dea57cad13eb0f47a8
Ghost <gabriel17@live.com.pt>
2020-06-02 15:31:21 +0100

    Updates

---
+++ precache-manifest.2366f9789cce223e65582660e6b053ad.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "84f658cebd873b2349608ac1f1f1fade",
+    "url": "/covidtracker/index.html"
+  },
+  {
+    "revision": "17a5fa1a196fdad3adfb",
+    "url": "/covidtracker/static/css/main.ddef87a5.chunk.css"
+  },
+  {
+    "revision": "c0255d6ff8bfc6e3643e",
+    "url": "/covidtracker/static/js/2.aee42d9f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/covidtracker/static/js/2.aee42d9f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "17a5fa1a196fdad3adfb",
+    "url": "/covidtracker/static/js/main.3edcf7a0.chunk.js"
+  },
+  {
+    "revision": "459ece35b3fed922ddf7",
+    "url": "/covidtracker/static/js/runtime-main.c868dbb1.js"
+  }
+]);
